0f9590e46c6e4deb1ea6ee323cf031e87c804214
Tamal kanti mazumder <65080702+striketm98@users.noreply.github.com>
2023-01-18 09:02:15 +0530

    Update ubuntu-gui_install.sh

--- ubuntu-gui_install.sh
+++ ubuntu-gui_install.sh
@@ -2,3 +2,12 @@
 sudo apt-get update -y   
 sudo apt-get install lxde -y
 sudo apt-get install xrdp -y
+sudo adduser xrdp ssl-cert
+
+
+
+# for ubuntu gui version 22.04 LTS
+
+echo gnome-session > ~/.xsession
+chmod +x ~/.xsession
+sudo reboot
