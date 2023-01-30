bd4109e2ae40c0a69e8c5c5d5ec477c05f35055a
Tamal kanti mazumder <65080702+striketm98@users.noreply.github.com>
2023-01-30 23:31:49 +0530

    Update static-ip.sh

--- static-ip.sh
+++ static-ip.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
+ip_add=
+ip_gateway=
 cat >> /etc/netplan/00-installer-config.yaml << EOF
 network:
   version: 2
   ethernets:
      ens33:
         dhcp4: false
-        addresses: [192.168.29.243/24]
+        addresses: [${ip_add}/24]
         routes:
         - to: default
-          via: 192.168.29.1
+          via: ${ip_gateway}
         nameservers:
           addresses: [8.8.8.8, 8.8.4.4]
   
